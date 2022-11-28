636d16759f2c799285d3e0d9bf532778307f0929
BSY <ideawolf@protonmail.com>
2022-11-28 16:48:38 +0900

    TextOverflow.Ellipsis

--- app/src/main/java/ideawolf/CookingAssistant/MainActivity.kt
+++ app/src/main/java/ideawolf/CookingAssistant/MainActivity.kt
@@ -21,10 +21,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -829,7 +831,7 @@ fun cooking_page(onNavigateToCooking: () -> Unit, onNavigateToHome: () -> Unit, 
                     ) {
                         Text(
                             "Current Cuisine:",
-                            fontSize = 20.sp,
+                            fontSize = 18.sp,
                             fontWeight = FontWeight.SemiBold
                         )
                         Spacer(Modifier.padding(2.dp))
@@ -850,7 +852,7 @@ fun cooking_page(onNavigateToCooking: () -> Unit, onNavigateToHome: () -> Unit, 
                         )
                         Text(
                             process_description.value,
-                            fontSize = 18.sp,
+                            fontSize = 16.sp,
                             textAlign = TextAlign.Center
                         )
                     }
@@ -936,8 +938,11 @@ fun FoodCardInProcess(cuisine: Cuisine) {
                     tint = Color.Unspecified,
                 )
             }
-            Text(cuisine.name)
-            Text("99.9" + " %")
+            Text(cuisine.name,
+                maxLines = 1,
+                softWrap = false,
+                overflow = TextOverflow.Ellipsis)
+            Text("99.9" + " %", maxLines = 1)
         }
         // Card content
     }
